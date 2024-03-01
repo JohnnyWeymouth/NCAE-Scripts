b81c0a4e8e19aeb63b3385af7451fe91e142c2df
JohnnyWeymouth <johnnycweymouth@gmail.com>
2024-03-01 14:39:23 -0700

    added backups script

--- send_ssh_keys.py
+++ send_ssh_keys.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 import os
-from all_functions import add_user_if_necessary, reset_user_password, recursively_create_dir
+from all_functions import add_user_if_necessary, recursively_create_dir
 from all_functions import create_blank_file_with_path, ssh_connection, recursively_change_owner
 from all_functions import change_permissions, copy_file_to_remote_host, copy_file_to_other_file
 import paramiko
@@ -12,8 +12,8 @@ def main():
     remote_user = os.getenv('REMOTE_USER')
     sudo_password = os.getenv('SUDO_PASSWORD')
 
-    designated_user = ''
-    local_path_of_pub_ssh_key = r''
+    designated_user = 'andrewanderson'
+    local_path_of_pub_ssh_key = r'C:\Users\johnn\OneDrive\Desktop\School\NCAE-Scripts\SSHKeys\andrewanderson_key.pub'
 
     with ssh_connection(remote_host_ip, remote_user, sudo_password) as ssh:
         set_up_ssh_for_user(ssh, designated_user, local_path_of_pub_ssh_key, 'abc123')
@@ -21,9 +21,6 @@ def main():
 def set_up_ssh_for_user(ssh:paramiko.SSHClient, designated_user:str, local_path_of_pub_ssh_key:str, sudo_password:str):
     # Add the user, if needed, to the remote host
     add_user_if_necessary(ssh, designated_user, sudo_password)
-
-    # Reset the password for that user to something strong
-    reset_user_password(ssh, designated_user, sudo_password)
 
     # Add the necessary dirs if not already created
     dir_path = f'/home/{designated_user}/.ssh/'
@@ -45,13 +42,10 @@ def set_up_ssh_for_user(ssh:paramiko.SSHClient, designated_user:str, local_path_
     # Change the files' and dirs' permissions
     dir_path = f'/home/{designated_user}/'
     change_permissions(ssh, dir_path, 755, sudo_password)
-
     dir_path = f'/home/{designated_user}/.ssh'
     change_permissions(ssh, dir_path, 700, sudo_password)
-
     file_path = f'/home/{designated_user}/.ssh/authorized_keys'
     change_permissions(ssh, file_path, 600, sudo_password)
-
     file_path = f'/home/{designated_user}/.ssh/known_hosts'
     change_permissions(ssh, file_path, 644, sudo_password)
 
